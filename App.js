853a94a1b320fae718c32a067d1bc00459db8206
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 13:02:33 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CarItem from "./components/CarItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <CarItem />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/CarItem/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const CarItem = () => {
+    return (
+        <View>
+            <Text>Rokas</Text>
+        </View>
+    );
+};
+
+export default CarItem;
