fde7813991e77bb620a5df6a49555309080f4e5b
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 12:52:24 +0300

    Rokas

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tesla app!</Text>
+      <Text>Tesla app! 🔥</Text>
       <StatusBar style="auto" />
     </View>
   );
