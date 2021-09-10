63d30aaa5fa8b8ed8d807e1eed904247ca00566b
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 14:39:57 +0300

    Rokas

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import styles from "./style";
 
 const Menu = () => {
     return (
-        <View>
+        <View style={styles.menuItems}>
             <Text>Something</Text>
         </View>
     );
--- components/Menu/style.js
+++ components/Menu/style.js
@@ -1,6 +1,9 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+    menuItems: {
+
+    },
 
 });
 
