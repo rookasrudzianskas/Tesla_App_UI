f3a12859f9a205c56fa61000d2b32831c3688309
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 15:01:02 +0300

    Rokas

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -5,19 +5,19 @@ import {Entypo, Feather} from "@expo/vector-icons";
 
 const Menu = () => {
     return (
-        <TouchableOpacity activeOpacity="0.8">
             <View style={styles.menuItems}>
-                <View style={styles.menuRow}>
-                    <Entypo name={`controller-next`} size={24} color="white" />
+                <TouchableOpacity activeOpacity="0.8">
+                        <View style={styles.menuRow}>
+                            <Entypo name={`controller-next`} size={24} color="white" />
 
-                    <View style={styles.menuTextBox}>
-                        <Text style={styles.menuText}>Media</Text>
-                    </View>
+                            <View style={styles.menuTextBox}>
+                                <Text style={styles.menuText}>Media</Text>
+                            </View>
 
-                    <Feather name="chevron-right" size={24} color="gray" />
-                </View>
+                            <Feather name="chevron-right" size={24} color="gray" />
+                        </View>
+                </TouchableOpacity>
             </View>
-        </TouchableOpacity>
     );
 };
 
