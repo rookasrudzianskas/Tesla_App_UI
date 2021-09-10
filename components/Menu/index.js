30099e69c54614fd389d9573724d113dd4df9d0a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 14:36:20 +0300

    Rokas

---
+++ components/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const Menu = () => {
+    return (
+        <View>
+            <Text>Something</Text>
+        </View>
+    );
+};
+
+export default Menu;
