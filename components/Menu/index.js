b205d7d93ac48cef9a01a55f5bf115803b6bb0b6
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 14:55:06 +0300

    Rokas

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
 import styles from "./style";
 import {Entypo, Feather} from "@expo/vector-icons";
 
 const Menu = () => {
     return (
-        <View style={styles.menuItems}>
-            <View style={styles.menuRow}>
-                <Entypo name={`controller-next`} size={24} color="white" />
+        <TouchableOpacity activeOpacity="0.8">
+            <View style={styles.menuItems}>
+                <View style={styles.menuRow}>
+                    <Entypo name={`controller-next`} size={24} color="white" />
 
-                <View style={styles.menuTextBox}>
-                    <Text style={styles.menuText}>Media</Text>
+                    <View style={styles.menuTextBox}>
+                        <Text style={styles.menuText}>Media</Text>
+                    </View>
+
+                    <Feather name="chevron-right" size={24} color="gray" />
                 </View>
-
-                <Feather name="chevron-right" size={24} color="gray" />
             </View>
-        </View>
+        </TouchableOpacity>
     );
 };
 
