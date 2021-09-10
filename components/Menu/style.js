dd6029d4a86817aecf68af1f1c5930366b7c53bd
Rokas <rookas.rudzenskas@gmail.com>
2021-09-10 14:50:55 +0300

    Rokas

--- components/Menu/style.js
+++ components/Menu/style.js
@@ -2,14 +2,15 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     menuItems: {
-
+        marginHorizontal: 10,
     },
     menuRow: {
-
+        flexDirection: 'row',
+        alignItems: 'center',
     },
 
     menuTextBox: {
-
+        flex: 1,
     },
     menuText: {
         color: 'white',
